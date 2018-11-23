853624b59815569100a8b6ebb91acd8ff4bf6269
GiannisKarastatiras <gianniskarastatiras@gmail.com>
2018-11-23 21:17:00 +0200

    Create PerformancesController

---
+++ EmployeeApp/Controllers/PerformancesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeApp.Controllers
+{
+    public class PerformancesController : Controller
+    {
+        // GET: Performances
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        
+    }
+}
