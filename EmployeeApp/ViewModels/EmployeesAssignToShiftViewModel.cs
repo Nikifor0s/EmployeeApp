127262bbba8074e325bbd81795a8bd056d3ea5ce
Nikifor0s <44725651+Nikifor0s@users.noreply.github.com>
2018-11-23 21:02:32 +0200

    ShiftController

---
+++ EmployeeApp/ViewModels/EmployeesAssignToShiftViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeApp.Models;
+using System.Collections.Generic;
+
+namespace EmployeeApp.ViewModels
+{
+    public class EmployeesAssignToShiftViewModel
+    {
+        public Shift Shift { get; set; }
+
+        public IEnumerable<Employee> Employees { get; set; }
+    }
+}
